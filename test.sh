76dbe9a7b34a5dcfb1a555a97b3994ca1790823f
HIGHER98 <hyerg@tcd.ie>
2019-03-28 03:10:09 +0000

    Update test.sh

--- test.sh
+++ test.sh
@@ -2,16 +2,7 @@
 
 gcc -O3 -msse4 conv-harness.c -fopenmp
 
-
-#./a.out 16 16 1 32 32
-#./a.out 16 16 3 32 32
-#./a.out 16 16 5 32 32
-#./a.out 16 16 7 32 32
-#./a.out 256 256 3 128 128
-#./a.out 512 512 1 128 32
-#./a.out 512 512 1 128 1024
-
-#Prapti's tests
+#Recommended tests
 ./a.out 16 16 5 1024 128
 ./a.out 32 32 3 512 256
 ./a.out 64 64 1 256 64
@@ -19,41 +10,89 @@ gcc -O3 -msse4 conv-harness.c -fopenmp
 ./a.out 256 256 5 64 64
 ./a.out 512 512 7 32 32
 
-#Gregg's conv time: 100597345 microseconds
-#Team conv time: 65248660 microseconds
-#./a.out 512 512 1 256 512
-
-#./a.out 512 512 3 256 512
-#./a.out 512 512 3 256 512
-#./a.out 512 512 3 256 512
-#./a.out 512 512 3 256 512
-#./a.out 512 512 3 256 512
-
-#Gregg's conv time: 100286075 microseconds
-#Team conv time: 63584369 microseconds
-#./a.out 512 512 1 512 256
-
-#./a.out 512 512 3 512 256
-#./a.out 512 512 3 512 256
-#./a.out 512 512 3 512 256
-#./a.out 512 512 3 512 256
-#./a.out 512 512 3 512 256
-
-#Gregg's conv time: 101653345 microseconds
-#Team conv time: 66276738 microseconds
-#./a.out 512 512 1 1024 128
 
 
-#./a.out 512 512 3 1024 128
-#./a.out 512 512 3 1024 128
-#./a.out 512 512 3 1024 128
-#./a.out 512 512 3 1024 128
-#./a.out 512 512 3 1024 128
-#./a.out 512 512 3 1024 128
+###All the below tests were ran on a shared 64 core server. I was running a htop simultaneously and could see other activities being ran on the server were causing CPU activity to fluctuate. For this reason these results are not 100% accurate
 
-#./a.out 512 512 1 2048 32
-#./a.out 512 512 3 2048 32
-#./a.out 512 512 3 2048 32
-#./a.out 512 512 3 2048 32
-#./a.out 512 512 3 2048 32
-#./a.out 512 512 3 2048 32
+
+
+###First round
+#Speedup: 17.363607
+#COMMENT: sum of absolute differences (0.000000)  within acceptable range (0.062500)
+
+#Speedup: 16.919711
+#COMMENT: sum of absolute differences (0.000000)  within acceptable range (0.062500)
+
+#Speedup: 5.656258
+#COMMENT: sum of absolute differences (0.000000)  within acceptable range (0.062500)
+
+#Speedup: 32.960659
+#COMMENT: sum of absolute differences (0.000000)  within acceptable range (0.062500)
+
+#Speedup: 30.806848
+#COMMENT: sum of absolute differences (0.000000)  within acceptable range (0.062500)
+
+#Speedup: 22.882252
+#COMMENT: sum of absolute differences (0.000000)  within acceptable range (0.062500)
+
+
+
+###Second round
+#Speedup: 18.764950
+#COMMENT: sum of absolute differences (0.000000)  within acceptable range (0.062500)
+
+#Speedup: 16.338502
+#COMMENT: sum of absolute differences (0.000000)  within acceptable range (0.062500)
+
+#Speedup: 9.354031
+#COMMENT: sum of absolute differences (0.000000)  within acceptable range (0.062500)
+
+#Speedup: 33.985529
+#COMMENT: sum of absolute differences (0.000000)  within acceptable range (0.062500)
+
+#Speedup: 40.238549
+#COMMENT: sum of absolute differences (0.000000)  within acceptable range (0.062500)
+
+#Speedup: 20.275225
+#COMMENT: sum of absolute differences (0.000000)  within acceptable range (0.062500)
+
+
+
+###Third round
+#Speedup: 14.276586
+#COMMENT: sum of absolute differences (0.000000)  within acceptable range (0.062500)
+
+#Speedup: 20.033951
+#COMMENT: sum of absolute differences (0.000000)  within acceptable range (0.062500)
+
+#Speedup: 6.592450
+#COMMENT: sum of absolute differences (0.000000)  within acceptable range (0.062500)
+
+#Speedup: 30.843405
+#COMMENT: sum of absolute differences (0.000000)  within acceptable range (0.062500)
+
+#Speedup: 22.841837
+#COMMENT: sum of absolute differences (0.000000)  within acceptable range (0.062500)
+
+#Speedup: 19.064867
+#COMMENT: sum of absolute differences (0.000000)  within acceptable range (0.062500)
+
+
+###Fourth round
+#Speedup: 16.299275
+#COMMENT: sum of absolute differences (0.000000)  within acceptable range (0.062500)
+
+#Speedup: 17.894892
+#COMMENT: sum of absolute differences (0.000000)  within acceptable range (0.062500)
+
+#Speedup: 6.767493
+#COMMENT: sum of absolute differences (0.000000)  within acceptable range (0.062500)
+
+#Speedup: 27.302812
+#COMMENT: sum of absolute differences (0.000000)  within acceptable range (0.062500)
+
+#Speedup: 51.157856
+#COMMENT: sum of absolute differences (0.000000)  within acceptable range (0.062500)
+
+#Speedup: 25.930428
+#COMMENT: sum of absolute differences (0.000000)  within acceptable range (0.062500)
