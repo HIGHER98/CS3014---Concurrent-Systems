27bb0e57f9bc72b94f8fbf78a489e40934bde58e
HIGHER98 <gary.james.hyer@gmail.com>
2019-03-28 02:53:45 +0000

    Added group chat tests

--- test.sh
+++ test.sh
@@ -3,30 +3,21 @@
 gcc -O3 -msse4 conv-harness.c -fopenmp
 
 
-#Gregg's conv time: 739 microseconds
-#Team conv time: 494 microseconds
-#./a.out 512 512 1 128 32
-
+#./a.out 16 16 1 32 32
 #./a.out 16 16 3 32 32
 #./a.out 16 16 5 32 32
 #./a.out 16 16 7 32 32
-
-
-#Gregg's conv time: 1581795 microseconds
-#Team conv time: 1000543 microseconds
-./a.out 256 256 3 128 128
-
-
-#Gregg's conv time: 101978515 microseconds
-#Team conv time: 64938434 microseconds
+#./a.out 256 256 3 128 128
+#./a.out 512 512 1 128 32
 #./a.out 512 512 1 128 1024
 
-
-#./a.out 512 512 3 128 1024
-#./a.out 512 512 3 128 1024
-#./a.out 512 512 3 128 1024
-#./a.out 512 512 3 128 1024
-#./a.out 512 512 3 128 1024
+#Prapti's tests
+./a.out 16 16 5 1024 128
+./a.out 32 32 3 512 256
+./a.out 64 64 1 256 64
+./a.out 128 128 7 128 128
+./a.out 256 256 5 64 64
+./a.out 512 512 7 32 32
 
 #Gregg's conv time: 100597345 microseconds
 #Team conv time: 65248660 microseconds
